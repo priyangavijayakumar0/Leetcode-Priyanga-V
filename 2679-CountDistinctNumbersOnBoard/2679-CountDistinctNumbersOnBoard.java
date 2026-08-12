fb768cf7aa5d64adc5c484f5194fa86c419a1961
priyangavijayakumar0 <priyangavijayakumar0@gmail.com>
2026-08-12 12:29:16 +0530

    Create 2679-CountDistinctNumbersOnBoard

---
+++ 2679-CountDistinctNumbersOnBoard/2679-CountDistinctNumbersOnBoard.java
@@ -0,0 +1,11 @@
+// Last updated: 8/12/2026, 12:29:16 PM
+class Solution {
+    public int distinctIntegers(int n) {
+        if(n==1)
+        {
+        return n;
+        }
+        return n-1;
+        
+    }
+}
