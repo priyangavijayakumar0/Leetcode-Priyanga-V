9ce2025c99cc254c06ec4602181c2b9bfe4f64e5
priyangavijayakumar0 <priyangavijayakumar0@gmail.com>
2026-08-12 12:30:47 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,14 @@
+// Last updated: 8/12/2026, 12:30:47 PM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
