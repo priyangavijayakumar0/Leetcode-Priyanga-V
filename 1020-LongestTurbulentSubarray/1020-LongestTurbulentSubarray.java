22ef517c1382c53697425f1f20fa6ca64f374886
priyangavijayakumar0 <priyangavijayakumar0@gmail.com>
2026-08-12 12:30:58 +0530

    Create 1020-LongestTurbulentSubarray

---
+++ 1020-LongestTurbulentSubarray/1020-LongestTurbulentSubarray.java
@@ -0,0 +1,29 @@
+// Last updated: 8/12/2026, 12:30:57 PM
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int n = arr.length;
+
+        if (n == 1) return 1;
+
+        int up = 1;
+        int down = 1;
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                up = down + 1;
+                down = 1;
+            } else if (arr[i] < arr[i - 1]) {
+                down = up + 1;
+                up = 1;
+            } else {
+                up = 1;
+                down = 1;
+            }
+
+            ans = Math.max(ans, Math.max(up, down));
+        }
+
+        return ans;
+    }
+}
