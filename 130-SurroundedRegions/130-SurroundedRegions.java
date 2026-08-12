dbf2897f62117c55727b1f99e35dbc535205cc0e
priyangavijayakumar0 <priyangavijayakumar0@gmail.com>
2026-08-12 12:35:39 +0530

    Create 130-SurroundedRegions

---
+++ 130-SurroundedRegions/130-SurroundedRegions.java
@@ -0,0 +1,38 @@
+// Last updated: 8/12/2026, 12:35:38 PM
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O')
+            return;
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
